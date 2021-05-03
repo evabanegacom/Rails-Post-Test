834795b143cf500f078fa36662cc1b26c6c7e3fc
Precious chiedu <udegbue69@gmail.com>
2021-05-03 16:58:00 +0100

    added validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :body, presence: true, allow_blank: false
+  validates :body, presence: true, allow_blank: false, length: { minimum: 5, maximum: 100,
+    too_long: '100 characters in body is the maximum allowed.' }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  validates :title, presence: true, allow_blank: false, length: { minimum: 5, maximum: 15,
+    too_long: '10 characters in body is the maximum allowed.' }
+  validates :body, presence: true, allow_blank: false, length: { minimum: 5, maximum: 50,
+    too_long: '50 characters in body is the maximum allowed.' }
 end
